20f90ab97c2676577cc523da964d964702456aa8
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-06-07 16:11:42 +0200

    aggiunta css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home primi passi</title>
+
+    <link rel="stylesheet" href="css/app.css">
+
 </head>
 <body>
+    <div class="container">
+        <h1>Ciao {{$nome}}</h1>
+        <h2>{{$sottotitolo}} {{$paese}} ?? </h2>
 
-    <h1>Ciao {{$nome}}</h1>
-    <h2>{{$sottotitolo}} {{$paese}} ?? </h2>
-
-    <a href="{{ route('info')}}">CLicca per maggiori info!</a>
+        <a href="{{ route('info')}}">CLicca per maggiori info!</a>
+    </div>
     
 </body>
 </html>
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -5,21 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>info-Paese</title>
+
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
+    <div class="info-container">
+        <h1>{{ $paese}}:</h1>
 
-    <h1>{{ $paese}}:</h1>
+        <h3>{{$info}}</h3>
 
-    <h3>{{$info}}</h3>
+        <ul>
+            <h4>Le frazioni del comune sono:</h4>
+            @foreach ( $frazioni as $frazione)
+                <li>{{ $frazione }}</li>
+            @endforeach
+        </ul>
 
-    <ul>
-        <h4>Le frazioni del comune sono:</h4>
-        @foreach ( $frazioni as $frazione)
-            <li>{{ $frazione }}</li>
-        @endforeach
-    </ul>
-
-    <a href="{{ route('home')}}">Torna alla home</a>
+        <a href="{{ route('home')}}">Torna alla home</a>
+    </div>
 
 
 
